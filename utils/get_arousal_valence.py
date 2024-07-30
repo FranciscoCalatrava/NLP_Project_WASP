c23a5463620edcec65bb05ff986ce4048d4b451b
FranciscoCalatrava <francisco.calatrava-nicolas@oru.se>
2024-07-30 18:21:00 +0200

    Almost finish the midi feature extraction. Still havinv problems with the list of instruments

--- utils/get_arousal_valence.py
+++ utils/get_arousal_valence.py
@@ -7,14 +7,6 @@ import sys
 
 
 def get_data(path):
-    # dir_names = [f for f in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, f))]
-    # all_files_names = []
-    # for a in dir_names:
-    #     files_names = [f for f in os.listdir(dataset_path+ f'/{a}') if os.path.isfile(os.path.join(dataset_path+ f'/{a}', f))]
-    #     for b in files_names:
-    #         all_files_names.append(b)
-    
-
     embeddings = {}
 
     with h5py.File(path+"/embeddings/embedding.h5", 'r') as hf:
@@ -25,12 +17,23 @@ def get_data(path):
             embeddings[a] = data
     return embeddings
     
-def get_models(embedding_model, prediction_model):
-    embedding_model = TensorflowPredictMusiCNN(graphFilename=embedding_model, output="model/dense/BiasAdd")
-    model = TensorflowPredict2D(graphFilename=prediction_model, output="model/Identity")
+def get_models(dataset, type):
+
+    sub_models = {'musicnn': 'msd-musicnn',
+                  'vgg': 'audioset-vggish'}
+
+    paths_embedding = f"./embedding_model/{dataset}/{sub_models[type]}/{sub_models[type]}.pb"
+    paths_prediction = f"./classifier_model/{dataset}/{sub_models[type]}/{dataset}-{sub_models[type]}-2.pb"
+
+    print(paths_embedding)
+    print(paths_prediction)
+
+    embedding_model = TensorflowPredictMusiCNN(graphFilename=paths_embedding, output="model/dense/BiasAdd")
+    model = TensorflowPredict2D(graphFilename=paths_prediction, output="model/Identity")
+
     return embedding_model, model
 
-def get_arousal_valence(embedding_model, model, data):
+def get_arousal_valence(embedding_model, model, data, dataset, type):
     labels = {}
     path = "dataset/midicap/midicaps/prepared/"
     for a in data.keys():
@@ -39,18 +42,10 @@ def get_arousal_valence(embedding_model, model, data):
         results = (results - 5) / 4
         labels[a] = results
 
-    print(labels)
-    with h5py.File(path+"labels/labels.h5", 'w') as hf:
+    with h5py.File(path+f"labels/labels_{dataset}_{type}.h5", 'w') as hf:
         for name in labels.keys() :
                     grp = hf.create_group(name)
                     grp.create_dataset('data', data=labels[name])
-
-
-    
-
-
-
-
 
 
 
@@ -58,13 +53,13 @@ def get_arousal_valence(embedding_model, model, data):
 if __name__ == "__main__":
 
     input_parameters = {"path": sys.argv[1],
-                        "embedding_model": sys.argv[2],
-                        "prediction_model": sys.argv[3]}
+                        "dataset": sys.argv[2],
+                        "type": sys.argv[3]}
 
     embeddings = get_data(input_parameters['path'])
 
-    embedding, prediction = get_models(input_parameters['embedding_model'], input_parameters['prediction_model'])
+    embedding, prediction = get_models(input_parameters['dataset'],input_parameters['type'])
 
-    get_arousal_valence(embedding, prediction, embeddings)
+    get_arousal_valence(embedding, prediction, embeddings, input_parameters['dataset'], input_parameters['type'])
 
 
